6cef22eb74d9cc4335581dff383c4901dc145fe4
Žan Jarc <zan.jarc@gmail.com>
2021-09-18 17:19:30 +0200

    Bang

--- SQL/Query.sql
+++ SQL/Query.sql
@@ -10,6 +10,7 @@ SET PUT_MID = (BID_PUT + ASK_PUT)/2
 UPDATE PRIMER_SPX
 SET CALL_PUT_ABS_DIFF = ABS(CALL_MID - PUT_MID)
 
+
 ------------------------poišèemo najmanjšo razliko za oba datuma----------------------
 
 SELECT *
@@ -17,7 +18,7 @@ FROM primer_spx
 INNER JOIN(
 	SELECT MIN(call_put_abs_diff) as min_diff
 	FROM primer_spx
-	where datum = '2020-03-31 00:00:00.000'
+	where datum = '2021-04-23 00:00:00.000'
 )t1
 ON t1.min_diff=primer_spx.call_put_abs_diff
 
@@ -31,34 +32,34 @@ INNER JOIN(
 ON t1.min_diff=primer_spx.call_put_abs_diff
 -----------------------------------------------------------------------------------------
 
----------F1 = 3086.80
---------F2=3086.65
+---------F1 = 3957,90
+--------F2=3957,65
  ---------------PUTS T1----------------------------
 SELECT [Datum],[Strike],[Puts],[Bid_Put],[Ask_Put]
 FROM primer_spx
-where  Datum = '2020-03-31 00:00:00.000'
-AND Strike < 3086.80
-ORDER BY Strike ASC
+where  Datum = '2021-04-26 00:00:00.000'
+AND Strike < 3957.90
+ORDER BY Strike DESC
 
 -------CALLS T1---------------
 SELECT [Datum],[Strike],[Calls],[Bid_Call],[Ask_Call]
 FROM primer_spx
-where  Datum = '2020-03-31 00:00:00.000'
-AND Strike > 3086.80
+where  Datum = '2021-04-26 00:00:00.000'
+AND Strike > 3957.90
 ORDER BY Strike ASC
 
 -----PUTS T2-------------------
 SELECT [Datum],[Strike],[Puts],[Bid_Put],[Ask_Put]
 FROM primer_spx
-where  Datum = '2020-04-06 00:00:00.000'
-AND Strike < 3086.65
-ORDER BY Strike ASC
+where  Datum = '2021-04-30 00:00:00.000'
+AND Strike < 3957.65
+ORDER BY Strike DESC
 
 -------------CALLS T2-------------
 SELECT [Datum],[Strike],[Calls],[Bid_Call],[Ask_Call]
 FROM primer_spx
-where  Datum = '2020-04-06 00:00:00.000'
-AND Strike > 3086.65
+where  Datum = '2021-04-30 00:00:00.000'
+AND Strike > 3957.65
 ORDER BY Strike ASC
 
 
